b3d061a94b3aecc3220ee5393fa2f29a90cbddeb
iceyang <w@icey.cc>
2018-03-28 21:11:08 +0800

    提交todos

--- pages/demo1/demo1.js
+++ pages/demo1/demo1.js
@@ -1,5 +1,24 @@
 Page({
   data:{
     text:"demo"
-  }
+  },
+  tapHandler(){
+    //
+    // wx.navigateTo({
+    //   url:'../demo2/demo2'
+    // })
+    // wx.redirectTo({
+    //   url:'../demo2/demo2'
+    // })
+    // wx.navigateBack()
+    wx.navigateBack({
+      delta:2 //返回几层  当delta很大时 返回最开始的页面
+    })
+
+    wx.navigateTo({})
+    wx.navigateBack({})
+    wx.redirectTo({})
+
+  },
+
 })
